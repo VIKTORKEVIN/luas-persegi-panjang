5479b24ae28229167f1763f02415016cc2eb3f60
VIKTORKEVIN <46395918+VIKTORKEVIN@users.noreply.github.com>
2019-01-05 16:56:32 +0700

    Add files via upload

---
+++ luas persegi panjang.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+    int main () {
+    int p,l,luas;
+
+    cout<<"masukkan panjang persegi panjang : ";
+    cin>>p;
+    cout<<"masukkan luas persegi panjang  : ";
+    cin>>l;
+
+    luas=p*l;
+    cout<<"luas persegi panjang adalah : "<<luas;
+    return 0;
+    }
